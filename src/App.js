864a7dbfad85300017628b92512f602f3ea7d3b5
tws9797 <twenseng7227@gmail.com>
2019-06-01 21:07:35 +0800

    Authentication first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View } from 'react-native' ;
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+
+class App extends Component{
+  componentWillMount(){
+  }
+
+  render(){
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        <LoginForm />
+      </View>
+    );
+  }
+}
+
+export default App;
