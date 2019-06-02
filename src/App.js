4684343fbdfc44d1b28351e64d093895eb335e04
tws9797 <twenseng7227@gmail.com>
2019-06-02 22:53:42 +0800

    Firebase auth

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import React, { Component } from 'react';
 import { View } from 'react-native' ;
 import { Header } from './components/common';
 import LoginForm from './components/LoginForm';
+import firebase from 'firebase';
 
 class App extends Component{
   componentWillMount(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyAeZNJBaYO08-__PAXiCQtr2Ci-cNdTko4",
+      authDomain: "authentication-50d10.firebaseapp.com",
+      databaseURL: "https://authentication-50d10.firebaseio.com",
+      projectId: "authentication-50d10",
+      storageBucket: "authentication-50d10.appspot.com",
+      messagingSenderId: "335621340857",
+      appId: "1:335621340857:web:3a9f121de8e13beb"
+    });
   }
 
   render(){
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component{
 
-  state = { email: '' , password: ''};
+  state = { email: '' , password: '', error: 'dsds'};
+
+  onButtonPress(){
+    const { email, password } = this.state;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .catch(() => {
+            this.setState({ error: 'Authentication Failed.'});
+          });
+      });
+  }
 
 
   render(){
@@ -29,14 +43,26 @@ class LoginForm extends Component{
           />
         </CardSection>
 
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Log in
           </Button>
         </CardSection>
 
       </Card>
     );
+  }
+}
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
   }
 }
 
